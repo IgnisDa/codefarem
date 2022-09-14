6104506896b418764f5089f22669e85c8c7db73f
Diptesh Choudhuri <ignisda2001@gmail.com>
2022-09-14 03:29:58 +0000

    feat: initial commit

---
+++ .devcontainer/scripts/post-create.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# setup the git identities
+git config --global user.name "${GIT_AUTHOR_NAME}"
+git config --global user.email "${GIT_AUTHOR_EMAIL}"
---
+++ apps/farem-rs/src/main.rs
@@ -0,0 +1,40 @@
+#![feature(proc_macro_hygiene, decl_macro, rustc_private, box_patterns)]
+
+use duct::cmd;
+use rocket::response::NamedFile;
+use rocket_contrib::json::Json;
+use serde::Deserialize;
+use std::io::Write;
+use tempfile::Builder;
+
+#[macro_use]
+extern crate rocket;
+
+#[derive(Debug, PartialEq, Eq, Deserialize)]
+struct FaremInput {
+    rust: String,
+}
+
+#[post("/farem", data = "<code_input>")]
+fn farem(code_input: Json<FaremInput>) -> Option<NamedFile> {
+    let mut builder = Builder::new();
+    builder.prefix("farem").suffix(".rs").rand_bytes(16);
+    let mut input_file = builder.tempfile().unwrap();
+    let output_file = builder.tempfile().unwrap();
+    write!(input_file, "{}", code_input.rust).unwrap();
+    cmd!(
+        "rustc",
+        input_file.path(),
+        "--target",
+        "wasm32-wasi",
+        "-o",
+        output_file.path()
+    )
+    .run()
+    .unwrap();
+    NamedFile::open(output_file).ok()
+}
+
+fn main() {
+    rocket::ignite().mount("/", routes![farem]).launch();
+}
