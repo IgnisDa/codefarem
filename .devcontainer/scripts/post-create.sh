7127801184dc062fbe350da501f6907841e9e8ae
Diptesh Choudhuri <ignisda2001@gmail.com>
2022-09-26 18:11:56 +0000

    ci: change devcontainer image to arch-linux

--- .devcontainer/scripts/post-create.sh
+++ .devcontainer/scripts/post-create.sh
@@ -4,19 +4,6 @@
 git config --global user.name "${GIT_AUTHOR_NAME}"
 git config --global user.email "${GIT_AUTHOR_EMAIL}"
 
-# install project specific tools
-rustup target add wasm32-wasi
-sudo apt install emscripten
-curl https://wasmtime.dev/install.sh -sSf | bash
-sudo cp ~/.wasmtime/bin/wasmtime /usr/bin/
-
-# install cargo tools
-_binstall_url="https://github.com/cargo-bins/cargo-binstall/releases/latest/download/cargo-binstall-x86_64-unknown-linux-musl.tgz"
-wget $_binstall_url -O /tmp/cargo-binstall.tgz
-tar zxvf /tmp/cargo-binstall.tgz
-sudo mv cargo-binstall /usr/bin/
-cargo binstall cargo-nextest cargo-watch --no-confirm
-
 # initialize edgedb project
 cd apps/farem-main
 edgedb project init --non-interactive
