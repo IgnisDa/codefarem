726e4dabe508408106bad0b06aef0a5e566b6c45
Diptesh Choudhuri <ignisda2001@gmail.com>
2022-10-02 10:57:34 +0000

    feat(auth): add role to the generated jwt playload

--- libs/auth/src/jwt.rs
+++ libs/auth/src/jwt.rs
@@ -1,7 +1,13 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Deserialize, Serialize)]
+pub enum Role {
+    User,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
 pub struct Claim {
     pub sub: String,
+    pub role: Role,
     pub exp: usize,
 }
