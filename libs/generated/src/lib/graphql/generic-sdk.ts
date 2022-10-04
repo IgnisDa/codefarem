2406e6ac726e33521d2b7c33a19f22bd145eac82
Diptesh Choudhuri <ignisda2001@gmail.com>
2022-10-04 07:40:34 +0000

    feat(website): error handling when submitting code

--- libs/generated/src/lib/graphql/generic-sdk.ts
+++ libs/generated/src/lib/graphql/generic-sdk.ts
@@ -18,19 +18,90 @@ export type Scalars = {
   Boolean: boolean;
   Int: number;
   Float: number;
+  UUID: string;
 };
+
+/** The result type if an error was encountered when executing code */
+export type ExecuteCodeError = {
+  __typename?: 'ExecuteCodeError';
+  /** The error that occurred while compiling/executing the submitted code */
+  error: Scalars['String'];
+  /** The step in which the error the above error occurred */
+  step: ExecuteCodeErrorStep;
+};
+
+/** The execution step in which an error was encountered */
+export enum ExecuteCodeErrorStep {
+  CompilationToWasm = 'COMPILATION_TO_WASM',
+  WasmExecution = 'WASM_EXECUTION',
+}
+
+/** The input object used to execute some code */
+export type ExecuteCodeInput = {
+  /** The code input that needs to be compiled */
+  code: Scalars['String'];
+  /** The language that needs to be compiled */
+  language: SupportedLanguage;
+};
+
+/** The result type if the code was compiled and executed successfully */
+export type ExecuteCodeOutput = {
+  __typename?: 'ExecuteCodeOutput';
+  /** The output of the code that was executed */
+  output: Scalars['String'];
+};
+
+/** The output object when executing code */
+export type ExecuteCodeResultUnion = ExecuteCodeError | ExecuteCodeOutput;
+
+/** The different errors that can occur when logging in to the service */
+export enum LoginError {
+  /** The credentials did not match */
+  CredentialsMismatch = 'CREDENTIALS_MISMATCH',
+}
+
+/** The result type if an error was encountered when creating a new user */
+export type LoginUserError = {
+  __typename?: 'LoginUserError';
+  /** The error encountered while logging in */
+  error: LoginError;
+};
+
+/** The input object used to create a new user */
+export type LoginUserInput = {
+  /** The email of the user */
+  email: Scalars['String'];
+  /** The password that the user wants to set */
+  password: Scalars['String'];
+};
+
+/** The result type if the user was created successfully */
+export type LoginUserOutput = {
+  __typename?: 'LoginUserOutput';
+  /** The unique JWT token to be issued */
+  token: Scalars['String'];
+};
+
+/** The output object when creating a new user */
+export type LoginUserResultUnion = LoginUserError | LoginUserOutput;
 
 /** The GraphQL top-level mutation type */
 export type MutationRoot = {
   __typename?: 'MutationRoot';
   /** Takes some code as input and compiles it to wasm before executing it */
-  executeCode: Scalars['String'];
+  executeCode: ExecuteCodeResultUnion;
+  /** Create a new user for the service */
+  registerUser: RegisterUserResultUnion;
 };
 
 /** The GraphQL top-level mutation type */
 export type MutationRootExecuteCodeArgs = {
-  input: Scalars['String'];
-  language: SupportedLanguage;
+  input: ExecuteCodeInput;
+};
+
+/** The GraphQL top-level mutation type */
+export type MutationRootRegisterUserArgs = {
+  input: RegisterUserInput;
 };
 
 /** The GraphQL top-level query type */
@@ -38,8 +109,12 @@ export type QueryRoot = {
   __typename?: 'QueryRoot';
   /** Get an example code snippet for a particular language */
   languageExample: Scalars['String'];
+  /** Login a user to the service */
+  loginUser: LoginUserResultUnion;
   /** Get a list of all the languages that the service supports. */
   supportedLanguages: Array<SupportedLanguage>;
+  /** Get information about the current user */
+  userDetails: UserDetailsResultUnion;
 };
 
 /** The GraphQL top-level query type */
@@ -47,20 +122,84 @@ export type QueryRootLanguageExampleArgs = {
   language: SupportedLanguage;
 };
 
+/** The GraphQL top-level query type */
+export type QueryRootLoginUserArgs = {
+  input: LoginUserInput;
+};
+
+/** The result type if an error was encountered when creating a new user */
+export type RegisterUserError = {
+  __typename?: 'RegisterUserError';
+  /** whether the provided email is unique */
+  emailNotUnique: Scalars['Boolean'];
+  /** whether the provided username is unique */
+  usernameNotUnique: Scalars['Boolean'];
+};
+
+/** The input object used to create a new user */
+export type RegisterUserInput = {
+  /** The email of the user */
+  email: Scalars['String'];
+  /** The password that the user wants to set */
+  password: Scalars['String'];
+  /** The username of the user */
+  username: Scalars['String'];
+};
+
+/** The result type if the user was created successfully */
+export type RegisterUserOutput = {
+  __typename?: 'RegisterUserOutput';
+  /** The ID of the user */
+  id: Scalars['UUID'];
+};
+
+/** The output object when creating a new user */
+export type RegisterUserResultUnion = RegisterUserError | RegisterUserOutput;
+
 export enum SupportedLanguage {
   Cpp = 'cpp',
   Go = 'go',
   Rust = 'rust',
 }
 
+/** The result type if an error was encountered when getting details of a user */
+export type UserDetailsError = {
+  __typename?: 'UserDetailsError';
+  /** The error describing what went wrong */
+  error: Scalars['String'];
+};
+
+/** The result type if details about the user were found successfully */
+export type UserDetailsOutput = {
+  __typename?: 'UserDetailsOutput';
+  /** Profile details about the user */
+  profile: UserProfileInformation;
+};
+
+/** The output object when creating a new user */
+export type UserDetailsResultUnion = UserDetailsError | UserDetailsOutput;
+
+export type UserProfileInformation = {
+  __typename?: 'UserProfileInformation';
+  /** The email of the user */
+  email: Scalars['String'];
+  /** The username of the user */
+  username: Scalars['String'];
+};
+
 export type ExecuteCodeMutationVariables = Exact<{
-  input: Scalars['String'];
-  language: SupportedLanguage;
+  input: ExecuteCodeInput;
 }>;
 
 export type ExecuteCodeMutation = {
   __typename?: 'MutationRoot';
-  executeCode: string;
+  executeCode:
+    | {
+        __typename: 'ExecuteCodeError';
+        error: string;
+        step: ExecuteCodeErrorStep;
+      }
+    | { __typename: 'ExecuteCodeOutput'; output: string };
 };
 
 export type SupportedLanguagesQueryVariables = Exact<{ [key: string]: never }>;
@@ -80,8 +219,17 @@ export type LanguageExampleQuery = {
 };
 
 export const ExecuteCodeDocument = gql`
-  mutation ExecuteCode($input: String!, $language: SupportedLanguage!) {
-    executeCode(input: $input, language: $language)
+  mutation ExecuteCode($input: ExecuteCodeInput!) {
+    executeCode(input: $input) {
+      __typename
+      ... on ExecuteCodeOutput {
+        output
+      }
+      ... on ExecuteCodeError {
+        error
+        step
+      }
+    }
   }
 `;
 export const SupportedLanguagesDocument = gql`
