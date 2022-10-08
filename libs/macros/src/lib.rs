c07ef9d6edf3363d0bc88dd5d5387674d256b194
Diptesh Choudhuri <ignisda2001@gmail.com>
2022-10-08 04:02:37 +0000

    feat(compilers): add endpoints to return errors and success using protobuf

--- libs/macros/src/lib.rs
+++ libs/macros/src/lib.rs
@@ -23,10 +23,15 @@ macro_rules! user_id_from_request {
 #[macro_export]
 macro_rules! main_function {
     ($example_handler:ident, $farem_handler:ident) => {
+        use actix_protobuf::{ProtoBuf, ProtoBufResponseBuilder};
         use actix_web::{error, middleware::Logger, web, App, HttpResponse, HttpServer};
         use log::info;
         use serde::Deserialize;
 
+        mod farem {
+            #![allow(clippy::derive_partial_eq_without_eq)]
+            include!(concat!(env!("OUT_DIR"), "/farem.rs"));
+        }
         #[derive(Debug, Deserialize)]
         struct FaremInput {
             code: String,
@@ -40,7 +45,31 @@ macro_rules! main_function {
             HttpServer::new(|| {
                 App::new()
                     .wrap(Logger::default())
+                    .route(
+                        "example-proto",
+                        web::get().to(|| async {
+                            HttpResponse::Ok().protobuf(farem::Example {
+                                data: $example_handler().await.into(),
+                            })
+                        }),
+                    )
                     .route("example", web::get().to($example_handler))
+                    .route(
+                        "farem-proto",
+                        web::post().to(|code: ProtoBuf<farem::Input>| async move {
+                            let resp = $farem_handler(&code.code);
+                            match resp {
+                                Ok(_) => HttpResponse::Ok().protobuf(farem::SuccessResponse {
+                                    data: resp.unwrap().into(),
+                                }),
+                                Err(_) => {
+                                    HttpResponse::BadRequest().protobuf(farem::ErrorResponse {
+                                        data: resp.unwrap_err().into(),
+                                    })
+                                }
+                            }
+                        }),
+                    )
                     .route(
                         "farem",
                         web::post().to(|code: web::Json<FaremInput>| async move {
