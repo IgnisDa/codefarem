dbd2df7ec533cf54ce4210916bd53d94c90ec162
Diptesh Choudhuri <ignisda2001@gmail.com>
2022-10-08 06:50:02 +0000

    feat: add protobuf server & client code generation

---
+++ apps/protos/src/bin/go.rs
@@ -0,0 +1,23 @@
+use duct::cmd;
+use macros::proto_server;
+use protos::{generate_input_and_output_files, run_command_and_capture_output};
+
+async fn example() -> &'static str {
+    include_str!("../../../../libs/examples/src/go/example.go").trim()
+}
+
+fn farem(code: &'_ str) -> Result<Vec<u8>, Vec<u8>> {
+    let (input_file_path, output_file_path) = generate_input_and_output_files("go", code);
+    let command = cmd!(
+        "tinygo",
+        "build",
+        "-wasm-abi=generic",
+        "-target=wasi",
+        "-o",
+        &output_file_path,
+        &input_file_path
+    );
+    run_command_and_capture_output(command, &output_file_path)
+}
+
+proto_server!(example, farem);
