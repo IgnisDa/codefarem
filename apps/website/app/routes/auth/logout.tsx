ea2cf1542eaa3de48486bf9661b5a1ef4ae78c69
Diptesh Choudhuri <ignisda2001@gmail.com>
2022-10-16 02:18:38 +0000

    fix(website): redirect to login page when logging out

--- apps/website/app/routes/auth/logout.tsx
+++ apps/website/app/routes/auth/logout.tsx
@@ -1,5 +1,3 @@
-import { redirect } from '@remix-run/node';
-
 import { FAILURE_REDIRECT_PATH, LOGOUT_PATH } from '../../lib/constants';
 import { authenticator } from '../../lib/services/auth.server';
 import { getFetchOptions, graphqlSdk } from '../../lib/services/graphql.server';
@@ -7,7 +5,6 @@ import { getFetchOptions, graphqlSdk } from '../../lib/services/graphql.server';
 import type { DataFunctionArgs } from '@remix-run/node';
 
 export const loader = async ({ request }: DataFunctionArgs) => {
-  if (!authenticator.isAuthenticated(request)) return redirect(LOGOUT_PATH);
   const user = await authenticator.isAuthenticated(request, {
     failureRedirect: FAILURE_REDIRECT_PATH,
   });
