419a11bdb4d3798e02bd3175d002f9c6ff5db8a5
Diptesh Choudhuri <ignisda2001@gmail.com>
2022-11-23 13:43:21 +0000

    fix(website): add connection header to all reqs
    Apparently due to changes in how node19 works as described in
    https://nodejs.org/en/blog/announcements/v19-release-announce/#http-s-1-1-keepalive-by-default,
    we need to add this header to fix the error: `FetchError: request
    to http://0.0.0.0:8000/graphql failed, reason: socket hang up`.

--- apps/website/app/lib/services/graphql.server.ts
+++ apps/website/app/lib/services/graphql.server.ts
@@ -2,7 +2,8 @@ import { GraphQLClient } from 'graphql-request';
 import { ApplicationConfig } from '../config.server';
 
 export const gqlClient = new GraphQLClient(
-  `${ApplicationConfig.APPLICATION_API_URL}/graphql`
+  `${ApplicationConfig.APPLICATION_API_URL}/graphql`,
+  { headers: { connection: 'keep-alive' } }
 );
 
 export const getAuthHeader = (token: string) => ({
