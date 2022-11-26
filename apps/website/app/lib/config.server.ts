368b79248e3213bf73829359b99c4eecea561eb9
Diptesh Choudhuri <ignisda2001@gmail.com>
2022-11-26 09:12:03 +0000

    chore: prefix env vars with project name
    This will let us distinguish application specific
    environment variables more effectively.

--- apps/website/app/lib/config.server.ts
+++ apps/website/app/lib/config.server.ts
@@ -1,11 +1,11 @@
 import * as env from 'env-var';
 
 const APPLICATION_API_URL = env
-  .get('APPLICATION_API_URL')
+  .get('WEBSITE_APPLICATION_API_URL')
   .required()
   .asString();
 const NODE_ENV = env.get('NODE_ENV').default('production').asString();
-const SESSION_SECRET_KEY = env.get('SESSION_SECRET_KEY').required().asString();
+const SESSION_SECRET_KEY = env.get('WEBSITE_SESSION_SECRET_KEY').required().asString();
 
 export const ApplicationConfig = {
   APPLICATION_API_URL,
