acee90b62afd2939cb67468b88e9947a0c910c3d
Diptesh Choudhuri <ignisda2001@gmail.com>
2022-11-26 05:52:06 +0000

    feat: add dockerfile generators for compilers proj

---
+++ apps/deployment/src/main.py
@@ -0,0 +1,43 @@
+import json
+from pathlib import Path
+
+import click
+from jinja2 import Environment, FileSystemLoader, StrictUndefined
+
+_base_dir = Path(__file__).parent / "templates"
+
+DATA_FILENAME = "data.json"
+
+compilers_environment = Environment(
+    loader=FileSystemLoader(_base_dir), undefined=StrictUndefined
+)
+
+
+@click.group()
+def cli():
+    """Generate docker-files for the various applications"""
+    pass
+
+
+@click.command()
+def compilers():
+    """Generate docker-files for the compilers"""
+    compilers_base = compilers_environment.get_template(
+        "compilers/base.Dockerfile"
+    )
+    with open(_base_dir / "compilers" / DATA_FILENAME) as f:
+        compilers_data = json.load(f)
+    executable_data = compilers_data["apps"]
+    for context in executable_data:
+        filename = compilers_data["dockerfile_path"].replace(
+            "{{ executable }}", context["EXECUTABLE_NAME"]
+        )
+        rendered = compilers_base.render(**context)
+        with open(filename, mode="w", encoding="utf-8") as dockerfile:
+            dockerfile.write(rendered)
+
+
+cli.add_command(compilers)
+
+if __name__ == "__main__":
+    cli()
---
+++ apps/deployment/src/templates/compilers/base.Dockerfile
@@ -0,0 +1,23 @@
+FROM lukemathwalker/cargo-chef:latest AS chef
+RUN rustup default nightly
+WORKDIR app
+
+FROM chef AS planner
+COPY . .
+RUN cargo chef prepare --recipe-path recipe.json
+
+FROM chef AS builder
+COPY --from=planner /app/recipe.json recipe.json
+RUN cargo chef cook --release --recipe-path recipe.json
+COPY . .
+RUN cargo build --release --bin {{ EXECUTABLE_NAME }} ;\
+    strip target/release/{{ EXECUTABLE_NAME }}
+
+FROM {{ IMAGE_NAME }} AS runtime
+{% for command in COMMANDS -%}
+RUN {{ command }}
+{%- endfor %}
+WORKDIR app
+ENV PORT=80
+COPY --from=builder /app/target/release/{{ EXECUTABLE_NAME }} /usr/local/bin/{{ EXECUTABLE_NAME }}
+ENTRYPOINT ["/usr/local/bin/{{ EXECUTABLE_NAME }}"]
