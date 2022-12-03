78444c61a5f4719bb6a04c256a0c6dffc7c4a0b5
Diptesh Choudhuri <ignisda2001@gmail.com>
2022-12-03 16:52:39 +0000

    feat(authenticator): add initial config file

---
+++ apps/authenticator/start.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+export DATABASE_USER=$(echo $DATABASE_URL | grep -oP "postgres://\K(.+?):" | cut -d: -f1)
+export DATABASE_PASSWORD=$(echo $DATABASE_URL | grep -oP "postgres://.*:\K(.+?)@" | cut -d@ -f1)
+export DATABASE_HOST=$(echo $DATABASE_URL | grep -oP "postgres://.*@\K(.+?):" | cut -d: -f1)
+export DATABASE_PORT=$(echo $DATABASE_URL | grep -oP "postgres://.*@.*:\K(\d+)/" | cut -d/ -f1)
+export DATABASE_NAME=$(echo $DATABASE_URL | grep -oP "postgres://.*@.*:.*/\K(.+?)$")
+
+mkdir -p /config
+
+envsubst < /config.yml > /config/config.yaml
+
+/hanko serve public
