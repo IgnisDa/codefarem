f182a0121cd3f91a03d9c6bb80e150ca320a8df1
Diptesh Choudhuri <ignisda2001@gmail.com>
2022-12-04 17:25:19 +0000

    feat(admin): setup mui

--- apps/admin/src/pages/_app.tsx
+++ apps/admin/src/pages/_app.tsx
@@ -1,7 +1,28 @@
-import "../styles/globals.css";
-import { type AppType } from "next/app";
-import { trpc } from "../utils/trpc";
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import createEmotionCache from 'src/libs/createEmotionCache';
+import { theme } from 'src/styles/theme';
 
-const MyApp: AppType = ({ Component, pageProps }) => <Component {...pageProps} />;
+const clientSideEmotionCache = createEmotionCache();
 
-export default trpc.withTRPC(MyApp);
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+export default function MyApp(props: MyAppProps) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
+}
