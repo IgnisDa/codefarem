30f8a1fa4ec490b37bd8deeee51b273351fe201b
Diptesh Choudhuri <ignisda2001@gmail.com>
2022-12-04 10:44:21 +0000

    feat(admin): instantiate db client globally

--- apps/admin/src/server/trpc/router/example.ts
+++ apps/admin/src/server/trpc/router/example.ts
@@ -1,6 +1,6 @@
 import e from ':main-db';
-import * as edgedb from 'edgedb';
 import { z } from 'zod';
+import { edgedb } from '../../db/client'
 
 import { publicProcedure, router } from '../trpc';
 
@@ -8,10 +8,10 @@ export const exampleRouter = router({
   hello: publicProcedure
     .input(z.object({ text: z.string().nullish() }).nullish())
     .query(async ({ input }) => {
-      const client = edgedb.createClient()
       const g = await e
         .select(e.organizations.Organization)
-        .run(client)
+        .run(edgedb)
+      console.log(g);
       return { greeting: `Hello ${input?.text ?? "world"}`, }
     }),
 });
