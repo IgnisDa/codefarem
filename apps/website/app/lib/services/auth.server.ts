311219a6eb1ce217323bd60e3282cffa0bf8ace4
Diptesh Choudhuri <ignisda2001@gmail.com>
2022-12-08 11:20:55 +0000

    feat(website): use new func to validate users

--- apps/website/app/lib/services/auth.server.ts
+++ apps/website/app/lib/services/auth.server.ts
@@ -4,18 +4,22 @@ import { decode } from 'jsonwebtoken';
 import { FAILURE_REDIRECT_PATH } from '../constants';
 import { extractHankoCookie } from './graphql.server';
 
-export async function requireHankoId(
-    request: Request,
+export type JwtPayload = { sub: string; exp: number; iat: number; }
+
+// Ensures the user has a hanko cookie and the cookie is not expired but does not verify
+// the signature.
+export async function requireValidJwt(request: Request,
     redirectTo: string = new URL(request.url).pathname
 ) {
     const hankoCookie = extractHankoCookie(request);
-    const decoded = decode(hankoCookie);
-    const hankoId = decoded?.sub;
-    if (!hankoId || typeof hankoId !== "string") {
+    const decoded = decode(hankoCookie) as JwtPayload;
+    const hankoId = decoded.sub;
+    const exp = (decoded.exp || 0) * 1000;
+    if (!decoded || !hankoId || exp < Date.now()) {
         const searchParams = new URLSearchParams([
             ["redirectTo", redirectTo],
         ]);
         throw redirect(`${FAILURE_REDIRECT_PATH}?${searchParams}`);
     }
-    return hankoId;
+    return decoded
 }
--- apps/website/app/routes/index.tsx
+++ apps/website/app/routes/index.tsx
@@ -1,22 +1,22 @@
 import { json } from '@remix-run/node';
 import { Link, useLoaderData } from '@remix-run/react';
 import { route } from 'routes-gen';
-import { requireHankoId } from '~/lib/services/auth.server';
+import { requireValidJwt } from '~/lib/services/auth.server';
 
 import type { LoaderArgs } from '@remix-run/node';
 
 export const loader = async ({ request }: LoaderArgs) => {
-  const hankoId = await requireHankoId(request);
-  return json({ hankoId });
+  const decoded = await requireValidJwt(request);
+  return json({ decoded });
 };
 
 export default () => {
-  const { hankoId } = useLoaderData<typeof loader>();
+  const { decoded } = useLoaderData<typeof loader>();
 
   return (
     <div>
       <h1>CodeFarem</h1>
-      <div>{hankoId}</div>
+      <div>{JSON.stringify(decoded)}</div>
       <div>
         <Link to={route('/auth/logout')}>Logout</Link>
       </div>
