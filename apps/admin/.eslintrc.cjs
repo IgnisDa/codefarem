212fafeb5d6d74c2068fbc12d42b0cac58ce8af7
Diptesh Choudhuri <ignisda2001@gmail.com>
2022-12-10 03:15:22 +0000

    build(admin): support imports from workspace projects

--- apps/admin/.eslintrc.cjs
+++ apps/admin/.eslintrc.cjs
@@ -1,5 +1,16 @@
 module.exports = {
   rules: {
     '@typescript-eslint/no-unsafe-assignment': 'off',
+    '@typescript-eslint/no-unsafe-member-access': 'off',
+    'import/extensions': [
+      'off',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
   },
 };
--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 import react from '@vitejs/plugin-react'
 
 // eslint-disable-next-line import/no-default-export
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    tsconfigPaths({ root: __dirname }),
+    react()
+  ]
 })
