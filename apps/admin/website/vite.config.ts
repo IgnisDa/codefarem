9154653bc7a60ebd675610836426c1430b867a40
Diptesh Choudhuri <ignisda2001@gmail.com>
2022-12-10 04:06:04 +0000

    feat(admin/website): change project location

--- apps/admin/website/vite.config.ts
+++ apps/admin/website/vite.config.ts
@@ -1,11 +1,15 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import react from '@vitejs/plugin-react'
 
+const dirname = path.dirname(fileURLToPath(import.meta.url))
+
 // eslint-disable-next-line import/no-default-export
 export default defineConfig({
   plugins: [
-    tsconfigPaths({ root: __dirname }),
+    tsconfigPaths({ root: dirname }),
     react()
   ]
 })
