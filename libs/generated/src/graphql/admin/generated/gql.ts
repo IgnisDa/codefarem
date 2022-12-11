e10da81d6b86cd214f8ea638cf36bc639ad98962
Diptesh Choudhuri <ignisda2001@gmail.com>
2022-12-11 03:38:55 +0000

    feat(generated): add mutation to create invite link

--- libs/generated/src/graphql/admin/generated/gql.ts
+++ libs/generated/src/graphql/admin/generated/gql.ts
@@ -2,7 +2,25 @@
 import * as types from './graphql';
 import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
 
-const documents = [];
+/**
+ * Map of all GraphQL operations in the project.
+ *
+ * This map has several performance disadvantages:
+ * 1. It is not tree-shakeable, so it will include all operations in the project.
+ * 2. It is not minifiable, so the string of a GraphQL query will be multiple times inside the bundle.
+ * 3. It does not support dead code elimination, so it will add unused operations.
+ *
+ * Therefore it is highly recommended to use the babel-plugin for production.
+ */
+const documents = {
+    "\n    mutation CreateInviteLink($input: CreateInviteLinkInput!) {\n      createInviteLink(input: $input) {\n        __typename\n        ...on CreateInviteLinkOutput {\n          token\n        }\n        ...on ApiError {\n          error\n        }\n      }\n    }\n": types.CreateInviteLinkDocument,
+};
+
+/**
+ * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
+export function graphql(source: "\n    mutation CreateInviteLink($input: CreateInviteLinkInput!) {\n      createInviteLink(input: $input) {\n        __typename\n        ...on CreateInviteLinkOutput {\n          token\n        }\n        ...on ApiError {\n          error\n        }\n      }\n    }\n"): (typeof documents)["\n    mutation CreateInviteLink($input: CreateInviteLinkInput!) {\n      createInviteLink(input: $input) {\n        __typename\n        ...on CreateInviteLinkOutput {\n          token\n        }\n        ...on ApiError {\n          error\n        }\n      }\n    }\n"];
+
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  *
--- libs/generated/src/graphql/admin/generated/graphql.ts
+++ libs/generated/src/graphql/admin/generated/graphql.ts
@@ -1,4 +1,5 @@
 /* eslint-disable */
+import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
 export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
@@ -13,6 +14,56 @@ export type Scalars = {
   Float: number;
 };
 
+/** The types of accounts a user can create */
+export enum AccountType {
+  Student = 'STUDENT',
+  Teacher = 'TEACHER'
+}
+
+/** An error type with an attached field to tell what went wrong */
+export type ApiError = {
+  /** The error describing what went wrong */
+  error: Scalars['String'];
+};
+
+export type CreateInviteLinkInput = {
+  /** The type of account to create */
+  accountType: AccountType;
+  /** The email address of the user to invite */
+  email?: InputMaybe<Scalars['String']>;
+  /** The time for which the invite link is valid */
+  validFor: Scalars['String'];
+};
+
+/** The result type if the invite link was created successfully */
+export type CreateInviteLinkOutput = {
+  /** The unique token associated with the invite link */
+  token: Scalars['String'];
+};
+
+/** The output object when creating a new class */
+export type CreateInviteLinkResultUnion = ApiError | CreateInviteLinkOutput;
+
+export type MutationRoot = {
+  createInviteLink: CreateInviteLinkResultUnion;
+  hello: Scalars['String'];
+};
+
+
+export type MutationRootCreateInviteLinkArgs = {
+  input: CreateInviteLinkInput;
+};
+
 export type QueryRoot = {
   hello: Scalars['String'];
 };
+
+export type CreateInviteLinkMutationVariables = Exact<{
+  input: CreateInviteLinkInput;
+}>;
+
+
+export type CreateInviteLinkMutation = { createInviteLink: { __typename: 'ApiError', error: string } | { __typename: 'CreateInviteLinkOutput', token: string } };
+
+
+export const CreateInviteLinkDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"mutation","name":{"kind":"Name","value":"CreateInviteLink"},"variableDefinitions":[{"kind":"VariableDefinition","variable":{"kind":"Variable","name":{"kind":"Name","value":"input"}},"type":{"kind":"NonNullType","type":{"kind":"NamedType","name":{"kind":"Name","value":"CreateInviteLinkInput"}}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"createInviteLink"},"arguments":[{"kind":"Argument","name":{"kind":"Name","value":"input"},"value":{"kind":"Variable","name":{"kind":"Name","value":"input"}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"__typename"}},{"kind":"InlineFragment","typeCondition":{"kind":"NamedType","name":{"kind":"Name","value":"CreateInviteLinkOutput"}},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"token"}}]}},{"kind":"InlineFragment","typeCondition":{"kind":"NamedType","name":{"kind":"Name","value":"ApiError"}},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"error"}}]}}]}}]}}]} as unknown as DocumentNode<CreateInviteLinkMutation, CreateInviteLinkMutationVariables>;
