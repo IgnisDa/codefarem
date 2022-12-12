1169e6e575f1ec8959488fa9a6e4190526b47cb1
Diptesh Choudhuri <ignisda2001@gmail.com>
2022-12-12 11:49:57 +0000

    refactor(auth): keep only exports in lib.rs

--- libs/auth/src/lib.rs
+++ libs/auth/src/lib.rs
@@ -1,39 +1,5 @@
 mod errors;
+mod utils;
 
 pub use errors::AuthError;
-use jwksclient2::keyset::KeyStore;
-use std::env;
-use utilities::{graphql::ApiError, users::AccountType};
-
-pub fn get_jwks_endpoint() -> String {
-    let authenticator_url = env::var("CODEFAREM_AUTHENTICATOR_URL").unwrap();
-    format!("{authenticator_url}/.well-known/jwks.json")
-}
-
-///.Returns the hanko ID from the authorization token
-pub async fn get_hanko_id_from_authorization_token(token: &str) -> Result<String, AuthError> {
-    let jwt = token
-        .strip_prefix("Bearer ")
-        .ok_or(AuthError::InvalidAuthHeader)?;
-    // TODO: Generate this beforehand and take it as an argument
-    let key_store = KeyStore::new_from(get_jwks_endpoint())
-        .await
-        .map_err(|_| AuthError::InvalidConfig)?;
-    let decoded = key_store
-        .verify(jwt)
-        .map_err(|_| AuthError::InvalidAuthHeader)?;
-    Ok(decoded.payload().sub().unwrap().to_string())
-}
-
-/// Checks if the user is allowed to access the resource
-pub fn validate_user_role(
-    required_account_type: &AccountType,
-    received_account_type: &AccountType,
-) -> Result<(), ApiError> {
-    if required_account_type != received_account_type {
-        return Err(ApiError {
-            error: format!("Only {required_account_type} can perform this action"),
-        });
-    }
-    Ok(())
-}
+pub use utils::{get_hanko_id_from_authorization_token, get_jwks_endpoint, validate_user_role};
