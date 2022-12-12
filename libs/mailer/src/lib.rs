231f1366443d874b2e59d42337e6bb2a4e789410
Diptesh Choudhuri <ignisda2001@gmail.com>
2022-12-12 14:13:26 +0000

    feat(mailer): add basic message builder

--- libs/mailer/src/lib.rs
+++ libs/mailer/src/lib.rs
@@ -1,34 +1,3 @@
-use anyhow::Result;
-use lettre::{transport::smtp::authentication::Credentials, SmtpTransport};
-use std::env;
+mod mail;
 
-pub struct Mailer {
-    mailer: SmtpTransport,
-}
-
-impl Mailer {
-    pub fn new() -> Result<Self> {
-        let smtp_host = env::var("SMTP_HOST")?;
-        let smtp_port = env::var("SMTP_PORT")?.parse::<u16>()?;
-        let smtp_username = env::var("SMTP_USER")?;
-        let smtp_password = env::var("SMTP_PASSWORD")?;
-
-        let credentials = Credentials::new(smtp_username, smtp_password);
-
-        let mailer = if cfg!(debug_assertions) {
-            SmtpTransport::builder_dangerous(&smtp_host)
-                .port(smtp_port)
-                .credentials(credentials)
-                .build()
-        } else {
-            SmtpTransport::relay(&smtp_host)
-                .unwrap()
-                .credentials(credentials)
-                .build()
-        };
-
-        mailer.test_connection()?;
-
-        Ok(Self { mailer })
-    }
-}
+pub use mail::Mailer;
---
+++ libs/mailer/src/mail.rs
@@ -0,0 +1,49 @@
+use anyhow::Result;
+use lettre::{
+    message::MessageBuilder, transport::smtp::authentication::Credentials, Message, SmtpTransport,
+};
+use std::env;
+
+fn message_settings() -> Result<MessageBuilder> {
+    let from_name = env::var("CODEFAREM_FROM_EMAIL_NAME")?;
+    let from_email = env::var("CODEFAREM_FROM_EMAIL")?;
+    let mailbox = format!("{from_name} <{from_email}>").parse().unwrap();
+    Ok(Message::builder().from(mailbox))
+}
+
+pub struct Mailer {
+    mailer: SmtpTransport,
+}
+
+impl Mailer {
+    pub fn new() -> Result<Self> {
+        let smtp_host = env::var("CODEFAREM_SMTP_HOST")?;
+        let smtp_port = env::var("CODEFAREM_SMTP_PORT")?.parse::<u16>()?;
+        let smtp_username = env::var("CODEFAREM_SMTP_USER")?;
+        let smtp_password = env::var("CODEFAREM_SMTP_PASSWORD")?;
+
+        let credentials = Credentials::new(smtp_username, smtp_password);
+
+        let mailer = if cfg!(debug_assertions) {
+            SmtpTransport::builder_dangerous(&smtp_host)
+                .port(smtp_port)
+                .credentials(credentials)
+                .build()
+        } else {
+            SmtpTransport::relay(&smtp_host)
+                .unwrap()
+                .credentials(credentials)
+                .build()
+        };
+
+        mailer.test_connection()?;
+
+        message_settings()?;
+
+        Ok(Self { mailer })
+    }
+}
+
+impl Mailer {
+    pub async fn create_invite_link() {}
+}
