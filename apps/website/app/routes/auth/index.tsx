e7fd1d9dc6d3f09ba57ecc2d30f2b45c1a7a47ae
Diptesh Choudhuri <ignisda2001@gmail.com>
2022-12-13 17:29:03 +0000

    feat(website): add input for invite token

--- apps/website/app/routes/auth/index.tsx
+++ apps/website/app/routes/auth/index.tsx
@@ -1,6 +1,6 @@
 import type { ActionArgs, LinksFunction } from '@remix-run/node';
 import { redirect } from '@remix-run/node';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { ClientOnly } from 'remix-utils';
 import { Hanko } from '@teamhanko/hanko-frontend-sdk';
 import { registerHankoAuth } from '~/lib/services/hanko.client';
@@ -11,7 +11,8 @@ import { z } from 'zod';
 import { zx } from 'zodix';
 import { gqlClient } from '~/lib/services/graphql.server';
 import { REGISTER_USER } from ':generated/graphql/orchestrator/mutations';
-import styles from './index.css';
+import styles from '../../styles/auth/index.css';
+import { Stack, TextInput } from '@mantine/core';
 
 export const links: LinksFunction = () => {
   return [{ rel: 'stylesheet', href: styles }];
@@ -20,34 +21,38 @@ export const links: LinksFunction = () => {
 const authSchema = z.object({
   email: z.string().email(),
   hankoId: z.string(),
-});
-
-const urlSchema = z.object({
   inviteToken: z.string().optional(),
 });
 
 export const action = async ({ request }: ActionArgs) => {
-  const { hankoId, email } = await zx.parseForm(request.clone(), authSchema);
+  const { hankoId, email, inviteToken } = await zx.parseForm(
+    request.clone(),
+    authSchema
+  );
   const { userWithEmail } = await gqlClient.request(USER_WITH_EMAIL, {
     input: { email },
   });
   if (userWithEmail.__typename === 'UserWithEmailError') {
-    const { inviteToken } = zx.parseQuery(request.clone(), urlSchema);
     const username = new Date().toISOString();
     await gqlClient.request(REGISTER_USER, {
-      input: { email, username, hankoId, inviteToken },
+      input: { email, username, hankoId, inviteToken: inviteToken || null },
     });
   }
   return redirect(SUCCESSFUL_REDIRECT_PATH);
 };
 
 export default () => {
+  const [inviteToken, setInviteToken] = useState('');
   const fetcher = useFetcher();
 
   const handler = async () => {
     const hanko = new Hanko(window.ENV.HANKO_URL);
     const user = await hanko.user.getCurrent();
-    const data = authSchema.parse({ hankoId: user.id, email: user.email });
+    const data = authSchema.parse({
+      hankoId: user.id,
+      email: user.email,
+      inviteToken: inviteToken,
+    });
     fetcher.submit(data, { method: 'post' });
   };
 
@@ -58,8 +63,15 @@ export default () => {
   });
 
   return (
-    <ClientOnly fallback={'Loading...'}>
-      {() => <hanko-auth lang="en" api={window.ENV.HANKO_URL} />}
-    </ClientOnly>
+    <Stack>
+      <ClientOnly fallback={'Loading...'}>
+        {() => <hanko-auth lang="en" api={window.ENV.HANKO_URL} />}
+      </ClientOnly>
+      <TextInput
+        label="Invite Token"
+        value={inviteToken}
+        onChange={(event) => setInviteToken(event.currentTarget.value)}
+      />
+    </Stack>
   );
 };
