6ef8b446d2c682488e125b9dcfe034204bd75137
Diptesh Choudhuri <ignisda2001@gmail.com>
2022-12-13 18:57:57 +0000

    feat(website): basic layout of questions create page

---
+++ apps/website/app/lib/components/QuestionProblem.tsx
@@ -0,0 +1,69 @@
+import { RichTextEditor, Link } from '@mantine/tiptap';
+import { useEditor } from '@tiptap/react';
+import Highlight from '@tiptap/extension-highlight';
+import StarterKit from '@tiptap/starter-kit';
+import Underline from '@tiptap/extension-underline';
+import TextAlign from '@tiptap/extension-text-align';
+import Superscript from '@tiptap/extension-superscript';
+import SubScript from '@tiptap/extension-subscript';
+
+export const QuestionProblem = () => {
+  const editor = useEditor({
+    extensions: [
+      StarterKit,
+      Underline,
+      Link,
+      Superscript,
+      SubScript,
+      Highlight,
+      TextAlign.configure({ types: ['heading', 'paragraph'] }),
+    ],
+    content: '',
+  });
+
+  return (
+    <RichTextEditor editor={editor}>
+      <RichTextEditor.Toolbar sticky stickyOffset={60}>
+        <RichTextEditor.ControlsGroup>
+          <RichTextEditor.Bold />
+          <RichTextEditor.Italic />
+          <RichTextEditor.Underline />
+          <RichTextEditor.Strikethrough />
+          <RichTextEditor.ClearFormatting />
+          <RichTextEditor.Highlight />
+          <RichTextEditor.Code />
+        </RichTextEditor.ControlsGroup>
+
+        <RichTextEditor.ControlsGroup>
+          <RichTextEditor.H1 />
+          <RichTextEditor.H2 />
+          <RichTextEditor.H3 />
+          <RichTextEditor.H4 />
+        </RichTextEditor.ControlsGroup>
+
+        <RichTextEditor.ControlsGroup>
+          <RichTextEditor.Blockquote />
+          <RichTextEditor.Hr />
+          <RichTextEditor.BulletList />
+          <RichTextEditor.OrderedList />
+          <RichTextEditor.Subscript />
+          <RichTextEditor.Superscript />
+        </RichTextEditor.ControlsGroup>
+
+        <RichTextEditor.ControlsGroup>
+          <RichTextEditor.Link />
+          <RichTextEditor.Unlink />
+        </RichTextEditor.ControlsGroup>
+
+        <RichTextEditor.ControlsGroup>
+          <RichTextEditor.AlignLeft />
+          <RichTextEditor.AlignCenter />
+          <RichTextEditor.AlignJustify />
+          <RichTextEditor.AlignRight />
+        </RichTextEditor.ControlsGroup>
+      </RichTextEditor.Toolbar>
+
+      <RichTextEditor.Content mt={10} />
+    </RichTextEditor>
+  );
+};
--- apps/website/app/routes/questions/create.tsx
+++ apps/website/app/routes/questions/create.tsx
@@ -1,36 +1,24 @@
-import { Listbox } from '@headlessui/react';
-// import {
-//   Button,
-//   Col,
-//   Container,
-//   Grid,
-//   Input,
-//   Row,
-//   Text,
-//   Textarea,
-// } from '@nextui-org/react';
-import { json, redirect } from '@remix-run/node';
-import { Form, useLoaderData } from '@remix-run/react';
-import { capitalCase } from 'change-case';
-import { set } from 'lodash';
-import { useState } from 'react';
-import { HiMinusCircle, HiPlusCircle } from 'react-icons/hi';
-import { notFound } from 'remix-utils';
-import { route } from 'routes-gen';
-import type { ActionArgs, LoaderArgs } from '@remix-run/node';
 import {
   AccountType,
   SupportedLanguage,
-  TestCaseUnit,
 } from ':generated/graphql/orchestrator/generated/graphql';
-import type { CreateQuestionInput } from ':generated/graphql/orchestrator/generated/graphql';
-import { authenticatedRequest, gqlClient } from '~/lib/services/graphql.server';
 import {
   CREATE_QUESTION,
   TEST_CASE_UNITS,
 } from ':generated/graphql/orchestrator/mutations';
-import { getUserDetails } from '~/lib/services/user.server';
+import { Container, Group, Stack, Text, TextInput } from '@mantine/core';
+import { json, redirect } from '@remix-run/node';
+import { useLoaderData } from '@remix-run/react';
+import { set } from 'lodash';
+import { useState } from 'react';
+import { notFound } from 'remix-utils';
+import { route } from 'routes-gen';
+import { QuestionProblem } from '~/lib/components/QuestionProblem';
 import { requireValidJwt } from '~/lib/services/auth.server';
+import { authenticatedRequest, gqlClient } from '~/lib/services/graphql.server';
+import { getUserDetails } from '~/lib/services/user.server';
+import type { ActionArgs, LoaderArgs } from '@remix-run/node';
+import type { CreateQuestionInput } from ':generated/graphql/orchestrator/generated/graphql';
 
 export async function loader({ request }: LoaderArgs) {
   await requireValidJwt(request);
@@ -70,154 +58,20 @@ export async function action({ request }: ActionArgs) {
   );
 }
 
-const SelectUnitCase = ({
-  name,
-  heading,
-  testCaseUnits,
-}: {
-  name: string;
-  heading: string;
-  testCaseUnits: TestCaseUnit[];
-}) => {
-  return (
-    <Listbox name={name} defaultValue={TestCaseUnit.String}>
-      {({ value }) => (
-        <>
-          <Text>{heading}</Text>
-          {/* TODO: use select from Next-UI once it has been released */}
-          <Listbox.Button>{capitalCase(value)}</Listbox.Button>
-          <Listbox.Options>
-            {testCaseUnits.map((unit, idx) => (
-              <Listbox.Option key={idx} value={unit}>
-                {capitalCase(unit)}
-              </Listbox.Option>
-            ))}
-          </Listbox.Options>
-        </>
-      )}
-    </Listbox>
-  );
-};
-
 export default () => {
   const { testCaseUnits } = useLoaderData<typeof loader>();
   // each element of this list denotes the number of [input, output] for a test case
   const [totalTestCases, setTotalTestCases] = useState([[1, 1]]);
 
   return (
-    <div>
-      <h1>Create Question</h1>
-      <Form method="post">
-        <Input name="name" type="text" required labelPlaceholder="Name" />
-        <Textarea
-          name="problem"
-          labelPlaceholder="Accompanying text"
-          placeholder="You can use github flavored markdown"
-          required
-        />
+    <Container w={'100%'} mx={{ xs: 10, md: 20 }}>
+      <Stack>
+        <TextInput label="Name" required />
         <div>
-          <Text h2>Test Cases</Text>
-          <Button
-            rounded
-            ghost
-            icon={<HiPlusCircle size={30} />}
-            onClick={() => setTotalTestCases([...totalTestCases, [1, 1]])}
-          >
-            Add test case
-          </Button>
-          <Grid.Container gap={2}>
-            {totalTestCases.map((testCases, tIdx) => (
-              <Grid key={tIdx}>
-                <Container>
-                  <Text h4>Input</Text>
-                  <HiPlusCircle
-                    size={30}
-                    onClick={() => {
-                      const newTestCases = [...totalTestCases];
-                      newTestCases[tIdx][0]++;
-                      setTotalTestCases(newTestCases);
-                    }}
-                  />
-                  {[...Array(testCases[0]).keys()].map((iIdx) => (
-                    <Row align="center" key={iIdx}>
-                      <Col>
-                        <SelectUnitCase
-                          name={`testCases[${tIdx}].inputs[${iIdx}].dataType`}
-                          heading="Type"
-                          testCaseUnits={testCaseUnits}
-                        />
-                      </Col>
-                      <Col>
-                        <Input
-                          name={`testCases[${tIdx}].inputs[${iIdx}].data`}
-                          type="text"
-                          label="Data"
-                        />
-                        <input
-                          name={`testCases[${tIdx}].inputs[${iIdx}].name`}
-                          type="text"
-                          defaultValue={`line${iIdx + 1}`}
-                          hidden
-                        />
-                      </Col>
-                      <Col>
-                        <HiMinusCircle
-                          size={30}
-                          onClick={() => {
-                            const newTestCases = [...totalTestCases];
-                            newTestCases[tIdx][0]--;
-                            setTotalTestCases(newTestCases);
-                          }}
-                        />
-                      </Col>
-                    </Row>
-                  ))}
-                  <Text h4>Output</Text>
-                  <HiPlusCircle
-                    size={30}
-                    onClick={() => {
-                      const newTestCases = [...totalTestCases];
-                      newTestCases[tIdx][1]++;
-                      setTotalTestCases(newTestCases);
-                    }}
-                  />
-                  {[...Array(testCases[1]).keys()].map((oIdx) => (
-                    <Row align="center" key={oIdx}>
-                      <Col>
-                        <SelectUnitCase
-                          name={`testCases[${tIdx}].outputs[${oIdx}].dataType`}
-                          heading="Type"
-                          testCaseUnits={testCaseUnits}
-                        />
-                      </Col>
-                      <Col>
-                        <Input
-                          name={`testCases[${tIdx}].outputs[${oIdx}].data`}
-                          type="text"
-                          label="Data"
-                        />
-                      </Col>
-                      <Col>
-                        <HiMinusCircle
-                          size={30}
-                          onClick={() => {
-                            const newTestCases = [...totalTestCases];
-                            newTestCases[tIdx][1]--;
-                            setTotalTestCases(newTestCases);
-                          }}
-                        />
-                      </Col>
-                    </Row>
-                  ))}
-                </Container>
-              </Grid>
-            ))}
-          </Grid.Container>
+          <Text>Problem statement</Text>
+          <QuestionProblem />
         </div>
-        <div>
-          <Button type="submit">Create Question</Button>
-        </div>
-      </Form>
-    </div>
+      </Stack>
+    </Container>
   );
 };
