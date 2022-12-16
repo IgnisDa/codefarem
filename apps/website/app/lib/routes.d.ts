cc83fd8c32eb7c78621e3017d320f4f4e304eff5
Diptesh Choudhuri <ignisda2001@gmail.com>
2022-12-16 16:48:11 +0000

    feat(website): add basic page for solving questions

--- apps/website/app/lib/routes.d.ts
+++ apps/website/app/lib/routes.d.ts
@@ -1,6 +1,7 @@
 declare module "routes-gen" {
   export type RouteParams = {
     "/questions/solve/:slug/:lang": { "slug": string, "lang": string };
+    "/questions/solve/:slug": { "slug": string };
     "/playground/:lang": { "lang": string };
     "/questions/create": Record<string, never>;
     "/classes/create": Record<string, never>;
@@ -14,6 +15,7 @@ declare module "routes-gen" {
   export function route<
     T extends
       | ["/questions/solve/:slug/:lang", RouteParams["/questions/solve/:slug/:lang"]]
+      | ["/questions/solve/:slug", RouteParams["/questions/solve/:slug"]]
       | ["/playground/:lang", RouteParams["/playground/:lang"]]
       | ["/questions/create"]
       | ["/classes/create"]
---
+++ apps/website/app/routes/questions/solve/$slug/index.tsx
@@ -0,0 +1,179 @@
+import { TestCaseUnit } from ':generated/graphql/orchestrator/generated/graphql';
+import { EXECUTE_CODE_FOR_QUESTION } from ':generated/graphql/orchestrator/mutations';
+import {
+  QUESTION_DETAILS,
+  SUPPORTED_LANGUAGES,
+} from ':generated/graphql/orchestrator/queries';
+import { json } from '@remix-run/node';
+import { useActionData, useLoaderData, useTransition } from '@remix-run/react';
+import Editor from '@monaco-editor/react';
+import { useState } from 'react';
+import { notFound } from 'remix-utils';
+import invariant from 'tiny-invariant';
+import { match } from 'ts-pattern';
+import { z } from 'zod';
+import { zx } from 'zodix';
+import { gqlClient } from '~/lib/services/graphql.server';
+import type { TestCaseFragment } from ':generated/graphql/orchestrator/generated/graphql';
+import { SupportedLanguage } from ':generated/graphql/orchestrator/generated/graphql';
+import type { LoaderArgs, ActionArgs, MetaFunction } from '@remix-run/node';
+import {
+  Accordion,
+  Button,
+  Code,
+  Text,
+  Container,
+  Flex,
+  Select,
+  Stack,
+  Box,
+} from '@mantine/core';
+import { IconDeviceFloppy, IconPlayerPlay } from '@tabler/icons';
+import { useEditor } from '@tiptap/react';
+import { RichTextEditor } from '@mantine/tiptap';
+import StarterKit from '@tiptap/starter-kit';
+
+export const meta: MetaFunction = ({ data }) => {
+  if (!data) return {};
+  return data.meta;
+};
+
+export async function loader({ params }: LoaderArgs) {
+  const { supportedLanguages } = await gqlClient.request(SUPPORTED_LANGUAGES);
+  const questionSlug = params.slug;
+  invariant(typeof questionSlug === 'string', 'Slug should be a string');
+  const { questionDetails } = await gqlClient.request(QUESTION_DETAILS, {
+    questionSlug,
+  });
+  if (questionDetails.__typename === 'ApiError')
+    throw notFound({ title: 'Not found' });
+  const meta = { title: `${questionDetails.name}` };
+  return json({
+    supportedLanguages,
+    questionDetails,
+    meta,
+    questionSlug,
+  });
+}
+
+export async function action({ request }: ActionArgs) {
+  const { input, language, questionSlug } = await zx.parseForm(request, {
+    input: z.string(),
+    language: z.string(),
+    questionSlug: z.string(),
+  });
+  const { executeCodeForQuestion } = await gqlClient.request(
+    EXECUTE_CODE_FOR_QUESTION,
+    {
+      input: {
+        questionSlug: questionSlug,
+        executeInput: {
+          arguments: [],
+          code: JSON.parse(input),
+          language: language as SupportedLanguage,
+        },
+      },
+    }
+  );
+  return json({ executeCodeForQuestion });
+}
+
+const DisplayData = (data: TestCaseFragment) => {
+  return match(data.unitType)
+    .with(TestCaseUnit.Number, TestCaseUnit.String, () =>
+      String(data.numberValue || data.stringValue)
+    )
+    .with(TestCaseUnit.NumberCollection, TestCaseUnit.StringCollection, () =>
+      (data.numberCollectionValue || data.stringCollectionValue || []).join(
+        ', '
+      )
+    )
+    .exhaustive();
+};
+
+export default () => {
+  const { supportedLanguages, questionDetails } =
+    useLoaderData<typeof loader>();
+  const editor = useEditor({
+    extensions: [StarterKit],
+    editable: false,
+    content: questionDetails.problem,
+  });
+  const actionData = useActionData<typeof action>();
+  const transition = useTransition();
+  const [language, setLanguage] = useState(SupportedLanguage.Cpp);
+  const [code, setCode] = useState('');
+
+  return (
+    <Container fluid mx={10}>
+      <Flex gap={20}>
+        <Stack w={'50%'}>
+          <RichTextEditor editor={editor}>
+            <RichTextEditor.Content />
+          </RichTextEditor>
+          <Accordion>
+            {questionDetails.testCases.map((testCase, idx) => {
+              const name = `Test Case ${idx + 1}`;
+              return (
+                <Accordion.Item key={idx} value={name}>
+                  <Accordion.Control>{name}</Accordion.Control>
+                  <Accordion.Panel>
+                    <Text>Inputs</Text>
+                    {testCase.inputs.map((input, idx) => (
+                      <Box key={idx}>
+                        <Code>
+                          {DisplayData(input.data as TestCaseFragment)}
+                        </Code>
+                      </Box>
+                    ))}
+                    <Text>Outputs</Text>
+                    {testCase.outputs.map((output, idx) => (
+                      <Box key={idx}>
+                        <Code>
+                          {DisplayData(output.data as TestCaseFragment)}
+                        </Code>
+                      </Box>
+                    ))}
+                  </Accordion.Panel>
+                </Accordion.Item>
+              );
+            })}
+          </Accordion>
+        </Stack>
+        <Stack w={'50%'}>
+          <Flex justify={'space-between'} align={'center'}>
+            <Select
+              data={supportedLanguages.map((l) => ({
+                label: l.toUpperCase(),
+                value: l,
+              }))}
+              value={language}
+              onChange={(e) => setLanguage(e as SupportedLanguage)}
+            />
+            <Button
+              variant={'default'}
+              leftIcon={<IconDeviceFloppy size={24} />}
+            >
+              Save
+            </Button>
+          </Flex>
+          <Editor
+            options={{ lineNumbers: 'off' }}
+            height={'50vh'}
+            language={language}
+            theme={'vs-dark'}
+            value={code}
+            onChange={(value) => setCode(value || '')}
+          />
+          <Flex justify={'space-between'} align={'center'}>
+            {/* TODO: Remove this element */}
+            <div></div>
+            <Button leftIcon={<IconPlayerPlay size={24} />} color={'blue'}>
+              Run Test Cases
+            </Button>
+          </Flex>
+        </Stack>
+      </Flex>
+    </Container>
+  );
+};
