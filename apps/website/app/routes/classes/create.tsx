b1c07e59aceab64633d4f22cddf0e72a0e3266b6
Diptesh Choudhuri <ignisda2001@gmail.com>
2022-12-16 17:21:21 +0000

    feat(website): use mantine for create class page

--- apps/website/app/routes/classes/create.tsx
+++ apps/website/app/routes/classes/create.tsx
@@ -5,20 +5,22 @@ import { route } from 'routes-gen';
 import { z } from 'zod';
 import { zx } from 'zodix';
 import type { ActionArgs, LoaderArgs } from '@remix-run/node';
-// import { Button, Input } from '@nextui-org/react';
 import { AccountType } from ':generated/graphql/orchestrator/generated/graphql';
 import { authenticatedRequest, gqlClient } from '~/lib/services/graphql.server';
 import { CREATE_CLASS } from ':generated/graphql/orchestrator/mutations';
 import { getUserDetails } from '~/lib/services/user.server';
+import { Button, Container, Paper, TextInput, Title } from '@mantine/core';
+import { requireValidJwt } from '~/lib/services/auth.server';
 
-export async function loader({ request }: LoaderArgs) {
+export const loader = async ({ request }: LoaderArgs) => {
+  await requireValidJwt(request);
   const userDetails = await getUserDetails(request);
   if (userDetails.accountType !== AccountType.Teacher)
     throw notFound({ message: 'Route not found' });
   return json({});
-}
+};
 
-export async function action({ request }: ActionArgs) {
+export const action = async ({ request }: ActionArgs) => {
   const { name } = await zx.parseForm(request.clone(), {
     name: z.string(),
   });
@@ -30,18 +32,20 @@ export async function action({ request }: ActionArgs) {
   if (createClass.__typename === 'ApiError')
     throw badRequest({ message: createClass.error });
   throw redirect(route('/classes/:id', { id: createClass.id }));
-}
+};
 
 export default () => {
   return (
-    <div>
-      <h1>Create Class</h1>
-      <Form method="post">
-        <Input name="name" type="text" required label="Name" />
-        <div>
-          <Button type="submit">Create Class</Button>
-        </div>
-      </Form>
-    </div>
+    <Container size={'xs'}>
+      <Title align="center">Create Class</Title>
+      <Paper withBorder shadow="md" p={25} mt={30} radius="md">
+        <Form method="post">
+          <TextInput label="Name" type={'text'} required name="name" />
+          <Button fullWidth mt="sm" type="submit">
+            Create
+          </Button>
+        </Form>
+      </Paper>
+    </Container>
   );
 };
