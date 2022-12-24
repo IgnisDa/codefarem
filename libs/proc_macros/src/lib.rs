0e8aaa9c46ae9cce2ba6aec87299e7bceaa5290b
Diptesh Choudhuri <ignisda2001@gmail.com>
2022-12-24 05:20:30 +0000

    feat(proc_macros): add macro to embed image as base64

---
+++ libs/proc_macros/src/lib.rs
@@ -0,0 +1,15 @@
+use proc_macro::TokenStream;
+use std::{env, path::PathBuf};
+
+#[proc_macro]
+pub fn embed_image_as_base64(image_path: TokenStream) -> TokenStream {
+    let root = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
+    let mut sanitized_image_path = image_path.to_string();
+    sanitized_image_path = sanitized_image_path.replace('\"', "");
+    let image_path = root.join("src").join(PathBuf::from(sanitized_image_path));
+    if !image_path.exists() {
+        panic!("Image path does not exist: {:?}", image_path);
+    }
+    let base64 = image_base64::to_base64(image_path.to_str().unwrap());
+    format!("\"{base64}\"").parse().unwrap()
+}
