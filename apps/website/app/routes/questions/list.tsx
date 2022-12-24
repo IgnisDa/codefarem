182a77c7b63a4b977d2e1c2f8f5fa2677dba945d
Diptesh Choudhuri <ignisda2001@gmail.com>
2022-12-24 04:05:53 +0000

    feat(website): add basic pagination

--- apps/website/app/routes/questions/list.tsx
+++ apps/website/app/routes/questions/list.tsx
@@ -1,23 +1,25 @@
 import { QUESTIONS_CONNECTION } from ':generated/graphql/orchestrator/queries';
 import {
+  Anchor,
   Button,
-  Card,
   Container,
   Flex,
-  Grid,
+  Select,
   Stack,
+  Table,
   Text,
   Title,
 } from '@mantine/core';
 import { json } from '@remix-run/node';
-import { useLoaderData } from '@remix-run/react';
+import { useLoaderData, useLocation, useNavigate } from '@remix-run/react';
 import { route } from 'routes-gen';
+import { getQuery, parseURL, withQuery } from 'ufo';
 import { z } from 'zod';
 import { zx } from 'zodix';
 import { gqlClient } from '~/lib/services/graphql.server';
 import type { LoaderArgs } from '@remix-run/node';
 
-const elementsPerPage = 4;
+const defaultElementsPerPage = 10;
 
 const schema = z.object({
   after: z.string().optional(),
@@ -30,7 +32,7 @@ export async function loader({ request }: LoaderArgs) {
   const { after, before, first, last } = zx.parseQuery(request, schema);
   const args = {
     first: !(first || last)
-      ? elementsPerPage
+      ? defaultElementsPerPage
       : first
       ? parseInt(first)
       : undefined,
@@ -47,44 +49,72 @@ export async function loader({ request }: LoaderArgs) {
 
 export default () => {
   const { allQuestions } = useLoaderData<typeof loader>();
+  const navigate = useNavigate();
+  const location = useLocation();
 
   return (
-    <Container size={'lg'} h={'100%'}>
-      <Flex direction={'column'} justify={'space-between'} h={'100%'}>
-        <Stack>
-          <Title>All Questions</Title>
-          <Grid gutter={'xl'}>
+    <Container h={'100%'}>
+      <Stack spacing={'xl'}>
+        <Title>All Questions</Title>
+        <Table fontSize={'lg'} withColumnBorders withBorder highlightOnHover>
+          <thead>
+            <tr>
+              <th>
+                <Text>ID</Text>
+              </th>
+              <th>
+                <Text>Name</Text>
+              </th>
+              <th>
+                <Text>Test cases</Text>
+              </th>
+              <th>
+                <Text>Created on</Text>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
             {allQuestions.edges.map(({ node }) => (
-              <Grid.Col sm={12} md={4} key={node.slug}>
-                <Card<'a'>
-                  shadow="sm"
-                  p="lg"
-                  radius="md"
-                  withBorder
-                  component="a"
-                  href={route('/questions/solve/:slug', { slug: node.slug })}
-                >
-                  <Stack>
-                    <Title order={3} underline>
-                      {node.name}
-                    </Title>
-                    <Text>
-                      Created on:{' '}
-                      {new Date(node.createdTime).toLocaleDateString()}
-                    </Text>
-                    <Text>Test cases: {node.numTestCases}</Text>
-                  </Stack>
-                </Card>
-              </Grid.Col>
+              <tr key={node.slug}>
+                <td>
+                  <Anchor
+                    href={route('/questions/solve/:slug', {
+                      slug: node.slug,
+                    })}
+                  >
+                    {node.slug}
+                  </Anchor>
+                </td>
+                <td>{node.name}</td>
+                <td>{node.numTestCases}</td>
+                <td>{new Date(node.createdTime).toLocaleDateString()}</td>
+              </tr>
             ))}
-          </Grid>
-        </Stack>
-        <Flex justify={'center'} gap={20}>
+          </tbody>
+        </Table>
+        <Flex justify={'end'} gap={20}>
+          <Select
+            defaultValue={defaultElementsPerPage.toString()}
+            onChange={(e) => {
+              const parsedUrl = parseURL(location.search);
+              const query = getQuery(parsedUrl.search);
+              if (!query.first && !query.last) query.first = e;
+              else if (query.first) query.first = e;
+              else if (query.last) query.last = e;
+              console.log(query);
+              navigate(withQuery(route('/questions/list'), query));
+            }}
+            data={[
+              { label: '10', value: '10' },
+              { label: '15', value: '15' },
+              { label: '20', value: '20' },
+            ]}
+          />
           <Button
             component={'a'}
             href={`${route('/questions/list')}?before=${
               allQuestions.pageInfo.startCursor
-            }&last=${elementsPerPage}`}
+            }&last=${defaultElementsPerPage}`}
             disabled={!allQuestions.pageInfo.hasPreviousPage}
           >
             Previous
@@ -93,13 +123,13 @@ export default () => {
             component={'a'}
             href={`${route('/questions/list')}?after=${
               allQuestions.pageInfo.endCursor
-            }&first=${elementsPerPage}`}
+            }&first=${defaultElementsPerPage}`}
             disabled={!allQuestions.pageInfo.hasNextPage}
           >
             Next
           </Button>
         </Flex>
-      </Flex>
+      </Stack>
     </Container>
   );
 };
