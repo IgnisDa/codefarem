d442ac2a258d8982c78912f6af8fb01ff62bbe11
Diptesh Choudhuri <ignisda2001@gmail.com>
2022-12-27 16:52:25 +0000

    feat(main-db): remove tests
    These tests were just testing the database functionality (and
    testing the technologies is something we never wanna do).
    Also deleted all the files that were generated by the query builder.

--- libs/main-db/tests/teardown.ts
+++
@@ -1,8 +0,0 @@
-import 'tsconfig-paths/register';
-import { execSync } from 'node:child_process';
-
-// eslint-disable-next-line import/no-default-export
-export default () => {
-  const database = process.env.EDGEDB_DATABASE
-  execSync(`edgedb query --instance main_db "DROP DATABASE ${database}"`);
-};
