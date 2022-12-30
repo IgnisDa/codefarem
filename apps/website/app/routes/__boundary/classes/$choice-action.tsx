c2259394c2096cc0a99a6653a3cb998f08a999ca
Diptesh Choudhuri <ignisda2001@gmail.com>
2022-12-30 09:42:19 +0000

    feat(website): add UI for handling goals

--- apps/website/app/routes/__boundary/classes/$choice-action.tsx
+++ apps/website/app/routes/__boundary/classes/$choice-action.tsx
@@ -1,8 +1,9 @@
-import { DateRangePicker } from '@mantine/dates';
 import { useFragment } from ':generated/graphql/orchestrator';
 import { AccountType } from ':generated/graphql/orchestrator/graphql';
 import { UPSERT_CLASS } from ':graphql/orchestrator/mutations';
+import dayjs from 'dayjs';
 import {
+  SEARCH_QUESTIONS,
   SEARCH_USER_DETAILS_FRAGMENT,
   SEARCH_USERS,
 } from ':graphql/orchestrator/queries';
@@ -18,30 +19,29 @@ import {
   Group,
   Input,
   MultiSelect,
+  Paper,
+  Select,
   Stack,
   Text,
   TextInput,
   Title,
 } from '@mantine/core';
-import { useDebouncedState } from '@mantine/hooks';
+import { DateRangePicker } from '@mantine/dates';
 import { json, redirect } from '@remix-run/node';
-import { Form, useFetcher, useLoaderData } from '@remix-run/react';
-import { forwardRef, useEffect, useState } from 'react';
+import { Form, useLoaderData } from '@remix-run/react';
+import { IconTrash } from '@tabler/icons';
+import { set } from 'lodash';
+import { forwardRef, useState } from 'react';
 import { badRequest } from 'remix-utils';
 import { route } from 'routes-gen';
-import { withQuery } from 'ufo';
-import { z } from 'zod';
-import { zx } from 'zodix';
 import { requireValidJwt } from '~/lib/services/auth.server';
 import { authenticatedRequest, gqlClient } from '~/lib/services/graphql.server';
 import { getUserDetails } from '~/lib/services/user.server';
 import { forbiddenError, verifyPageAction } from '~/lib/utils';
-import type { SearchLoader } from '~/routes/api/searchQuestion';
 import type { SearchUserDetailsFragment } from ':generated/graphql/orchestrator/graphql';
 import type { ActionArgs, LoaderArgs } from '@remix-run/node';
 import type { FragmentType } from ':generated/graphql/orchestrator';
 import type { ComponentPropsWithoutRef } from 'react';
-import { IconTrash } from '@tabler/icons';
 
 const convertFromFragmentList = (
   list: FragmentType<typeof SEARCH_USER_DETAILS_FRAGMENT>[],
@@ -70,6 +70,13 @@ const MultiSelectItem = forwardRef<HTMLDivElement, ItemProps>(
   )
 );
 
+type Goal = {
+  name: string;
+  color: string;
+  dateRange: [string, string];
+  questionInstances: { id: string; numTestCases: number; name: string }[];
+};
+
 export const loader = async ({ request, params }: LoaderArgs) => {
   const action = verifyPageAction(params);
   await requireValidJwt(request);
@@ -77,6 +84,14 @@ export const loader = async ({ request, params }: LoaderArgs) => {
   if (userDetails.accountType !== AccountType.Teacher) forbiddenError();
   const { searchUsers } = await gqlClient.request(SEARCH_USERS, { input: {} });
 
+  const { searchQuestions } = await gqlClient.request(SEARCH_QUESTIONS, {
+    input: { queryString: '' },
+  });
+  const allQuestions = searchQuestions.results.map((s) => ({
+    value: s.id,
+    label: s.name,
+    numTestCases: s.numTestCases,
+  }));
   const students = convertFromFragmentList(
     searchUsers.students,
     AccountType.Student
@@ -85,7 +100,7 @@ export const loader = async ({ request, params }: LoaderArgs) => {
     searchUsers.teachers,
     AccountType.Teacher
   );
-  const defaultGoals = [
+  const defaultGoals: Goal[] = [
     {
       name: 'Learn the basics',
       color: '#7b9c30',
@@ -93,25 +108,33 @@ export const loader = async ({ request, params }: LoaderArgs) => {
       questionInstances: [],
     },
   ];
-  return json({ students, teachers, defaultGoals, action });
+  return json({ students, teachers, defaultGoals, action, allQuestions });
 };
 
 export const action = async ({ request }: ActionArgs) => {
-  const { name, studentsData, teachersData, selectedQuestions } =
-    await zx.parseForm(request.clone(), {
-      name: z.string(),
-      // the `MultiSelect` component returns a comma separated string of values
-      studentsData: z.string(),
-      teachersData: z.string(),
-      selectedQuestions: z.string(),
-    });
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData.entries());
+  const input = {} as any;
+  Object.entries(data).forEach(([key, value]) => {
+    let newValue: any = value;
+    if (key === 'teacherIds' || key === 'studentIds')
+      newValue = value.toString().split(',').filter(Boolean);
+    // range in the format of `'30 Dec – 31 Dec'`, needs to be converted to iso string
+    else if (key.includes('range')) {
+      const ranges = value.toString().split(' – ');
+      newValue = {
+        start: dayjs(ranges[0], 'DD MMM').toISOString(),
+        end: dayjs(ranges[1], 'DD MMM').toISOString(),
+      };
+    }
+    set(input, key, newValue);
+  });
 
-  const teacherIds = teachersData.split(',').filter(Boolean);
-  const studentIds = studentsData.split(',').filter(Boolean);
+  console.dir(input, { depth: Infinity });
 
   const { upsertClass } = await gqlClient.request(
     UPSERT_CLASS,
-    { input: { name, teacherIds, studentIds } },
+    { input },
     authenticatedRequest(request)
   );
   if (upsertClass.__typename === 'ApiError')
@@ -120,31 +143,10 @@ export const action = async ({ request }: ActionArgs) => {
 };
 
 export default () => {
-  const { students, teachers, defaultGoals } = useLoaderData<typeof loader>();
-  const fetcher = useFetcher<SearchLoader>();
-  const [searchQuestion, setSearchQuestion] = useDebouncedState('', 300);
-  const [selectedQuestions, setSelectedQuestions] = useState(
-    new Set<{ label: string; value: string; numTestCases: number }>()
-  );
-  const [goals, setGoals] = useState<
-    {
-      name: string;
-      color: string;
-      dateRange: [string, string];
-      questionInstances: string[];
-    }[]
-  >(defaultGoals);
+  const { students, teachers, defaultGoals, allQuestions } =
+    useLoaderData<typeof loader>();
 
-  useEffect(() => {
-    if (searchQuestion) {
-      fetcher.load(
-        withQuery(route('/api/searchQuestion'), {
-          search: searchQuestion,
-        })
-      );
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [searchQuestion]);
+  const [goals, setGoals] = useState(defaultGoals);
 
   return (
     <Container size={780}>
@@ -160,7 +162,7 @@ export default () => {
               placeholder={'A name for the class'}
             />
             <MultiSelect
-              name="teachersData"
+              name="teacherIds"
               data={teachers}
               label={'Teachers'}
               placeholder="The teachers of the class, can be modified later"
@@ -168,7 +170,7 @@ export default () => {
               searchable
             />
             <MultiSelect
-              name="studentsData"
+              name="studentIds"
               data={students}
               label={'Students'}
               placeholder="The students of the class, can be modified later"
@@ -183,12 +185,13 @@ export default () => {
             >
               <Stack mt={'xs'}>
                 {goals.map((goal, i) => (
-                  <Card shadow="sm" radius="md" withBorder key={i}>
+                  <Paper shadow="sm" radius="md" p={'md'} withBorder key={i}>
                     <Stack>
                       <Group>
                         <TextInput
                           label="Name"
                           required
+                          name={`goals.${i}.name`}
                           value={goal.name}
                           onChange={(e) => {
                             setGoals((prev) => {
@@ -200,64 +203,92 @@ export default () => {
                         />
                         <DateRangePicker
                           label="Date range"
+                          name={`goals.${i}.range`}
                           required
                           value={[
                             new Date(goal.dateRange[0]),
                             new Date(goal.dateRange[1]),
                           ]}
-                          inputFormat="DD/MM/YYYY"
+                          inputFormat="DD MMM"
                           dropdownType={'modal'}
+                          onChange={(e) => {
+                            if (e)
+                              setGoals((prev) => {
+                                const newGoals = [...prev];
+                                const [start, end] = e;
+                                newGoals[i].dateRange[0] =
+                                  start?.toISOString() || '';
+                                newGoals[i].dateRange[1] =
+                                  end?.toISOString() || '';
+                                return newGoals;
+                              });
+                          }}
                         />
-                        <ColorInput label="Color" required value={goal.color} />
+                        <ColorInput
+                          label="Color"
+                          required
+                          value={goal.color}
+                          name={`goals.${i}.color`}
+                          onChange={(e) => {
+                            setGoals((prev) => {
+                              const newGoals = [...prev];
+                              newGoals[i].color = e;
+                              return newGoals;
+                            });
+                          }}
+                        />
                       </Group>
-                      <MultiSelect
-                        data={[
-                          ...selectedQuestions,
-                          ...(fetcher.data?.data || []),
-                        ]}
+                      <Select
+                        data={allQuestions}
                         required
-                        name="selectedQuestions"
                         label="Questions for this goal"
                         placeholder="Start typing to search for questions"
                         searchable
                         clearable
-                        onSearchChange={setSearchQuestion}
-                        onChange={(values) => {
-                          setSelectedQuestions((prev) => {
-                            const newSet = new Set(prev);
-                            values.forEach((a) => {
-                              const data = fetcher.data?.data?.find(
-                                (b) => b.value === a
-                              );
-                              if (data) newSet.add(data);
-                            });
-                            return newSet;
+                        onChange={(selected) => {
+                          setGoals((prev) => {
+                            const newGoals = [...prev];
+                            const question = allQuestions.find(
+                              (q) => q.value === selected
+                            );
+                            if (!question) return newGoals;
+                            const newQuestion = {
+                              id: question.value,
+                              numTestCases: question.numTestCases,
+                              name: question.label,
+                            };
+                            newGoals[i].questionInstances.push(newQuestion);
+                            return newGoals;
                           });
                         }}
                       />
-                      {selectedQuestions.size > 0 && (
+                      {goal.questionInstances.length > 0 && (
                         <Flex gap={15} wrap={'wrap'}>
-                          {Array.from(selectedQuestions).map((q) => (
-                            <Card
-                              shadow="sm"
-                              radius="md"
-                              withBorder
-                              key={q.value}
-                            >
+                          {goal.questionInstances.map((q, idx) => (
+                            <Card shadow="sm" radius="md" withBorder key={q.id}>
+                              <input
+                                type="hidden"
+                                name={`goals.${i}.questionInstances[${idx}]]`}
+                                value={q.id}
+                              />
                               <Group position="apart">
                                 <ActionIcon
                                   color={'red'}
                                   onClick={() => {
-                                    setSelectedQuestions((prev) => {
-                                      const newSet = new Set(prev);
-                                      newSet.delete(q);
-                                      return newSet;
+                                    setGoals((prev) => {
+                                      const newGoals = [...prev];
+                                      newGoals[i].questionInstances = newGoals[
+                                        i
+                                      ].questionInstances.filter(
+                                        (qi) => qi.id !== q.id
+                                      );
+                                      return newGoals;
                                     });
                                   }}
                                 >
                                   <IconTrash />
                                 </ActionIcon>
-                                <Text weight={500}>{q.label}</Text>
+                                <Text weight={500}>{q.name}</Text>
                                 <Badge color="pink" variant="light">
                                   {q.numTestCases} test case
                                   {q.numTestCases !== 1 && 's'}
@@ -268,8 +299,26 @@ export default () => {
                         </Flex>
                       )}
                     </Stack>
-                  </Card>
+                  </Paper>
                 ))}
+                <Button
+                  compact
+                  variant={'default'}
+                  onClick={() => {
+                    const newGoal = {
+                      name: '',
+                      color: '',
+                      dateRange: ['2022-12-30', '2022-12-31'] as [
+                        string,
+                        string
+                      ],
+                      questionInstances: [],
+                    };
+                    setGoals((prev) => [...prev, newGoal]);
+                  }}
+                >
+                  Add goal
+                </Button>
               </Stack>
             </Input.Wrapper>
             <Divider variant={'dashed'} />
